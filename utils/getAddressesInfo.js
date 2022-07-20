73a403f52e99400b1f36a169776cd231eeb62891
Tamir Yirga <41894752+tamirkifle@users.noreply.github.com>
2022-07-20 15:26:52 +0300

    updated getAddressesInfo: fixed missing XpubsInfo

--- utils/getAddressesInfo.js
+++ utils/getAddressesInfo.js
@@ -2,7 +2,8 @@ const { ShelleyBaseAddressProvider } = require("./addressProvider");
 const { bech32 } = require("cardano-crypto.js");
 const getConfig = require("./config");
 const getCryptoProvider = require("./cryptoProvider");
-const axios = require('axios').default;
+const { writeXpubDataToSession } = require("./firestore");
+const fetch = require("cross-fetch");
 
 const AddressGeneratorFactory = (cryptoProvider, type) => async (index) => {
   //type 0 for external 1 for internal
@@ -10,19 +11,18 @@ const AddressGeneratorFactory = (cryptoProvider, type) => async (index) => {
     .address;
 };
 const getAddressSummary = async (address) => {
-
   // const fetchWithRetries = async (
-  //   url, 
-  //   options, 
+  //   url,
+  //   options,
   //   retryCount = 0,
   // ) => {
-  //   // split out the maxRetries option from the remaining 
+  //   // split out the maxRetries option from the remaining
   //   // options (with a default of 3 retries)
   //   const { maxRetries = 3, ...remainingOptions } = options;
   //   try {
   //     return await fetch(url, remainingOptions);
   //   } catch (error) {
-  //     // if the retryCount has not been exceeded, call again 
+  //     // if the retryCount has not been exceeded, call again
   //     if (retryCount < maxRetries) {
   //       return fetchWithRetries(url, options, retryCount + 1);
   //     }
@@ -30,9 +30,9 @@ const getAddressSummary = async (address) => {
   //     throw error;
   //   }
   // }
-  const summary = await axios.get(
-    `https://explorer-api.testnet.dandelion.link/api/addresses/summary/${address}`, 
-  ).then(res => {console.log(res.data); return res.data});
+  const summary = await fetch(
+    `https://explorer-api.testnet.dandelion.link/api/addresses/summary/${address}`
+  ).then((res) => res.json());
   const { caTxNum, caBalance, caTxList } = summary.Right;
   return {
     isUsed: caTxNum > 0,
@@ -41,42 +41,54 @@ const getAddressSummary = async (address) => {
   };
 };
 const getAddressSummaries = async (addresses) => {
-
-  const axiosPostWithRetries = async (
-    url, 
+  const postObjectAsJsonWithRetries = async (
+    url,
     object,
     maxRetries = 3,
-    retryCount = 0,
+    retryCount = 0
   ) => {
-    // split out the maxRetries option from the remaining 
+    // split out the maxRetries option from the remaining
     // options (with a default of 3 retries)
     try {
-      return await axios.post(url, object);
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(object),
+      };
+      return await fetch(url, requestOptions).then((res) => res.json());
     } catch (error) {
-      // if the retryCount has not been exceeded, call again 
+      // if the retryCount has not been exceeded, call again
       if (retryCount < maxRetries) {
-        return axiosPostWithRetries(url, object,maxRetries, retryCount + 1);
+        return postObjectAsJsonWithRetries(
+          url,
+          object,
+          maxRetries,
+          retryCount + 1
+        );
       }
       // max retries exceeded
       throw error;
     }
-  }
-  const summary = await axiosPostWithRetries(
-    `https://explorer-testnet.adalite.io/api/bulk/addresses/summary`, addresses, 
-  ).then(res => res.data);
+  };
+  const summary = await postObjectAsJsonWithRetries(
+    `https://explorer-testnet.adalite.io/api/bulk/addresses/summary`,
+    addresses
+  );
   const { caBalance, caTxList } = summary.Right;
   const addressesInTxns = caTxList.reduce((acc, tx) => {
-
-    const addrArray = tx.ctbInputs.map(input => input[0]).concat(tx.ctbOutputs.map(output => output[0]));
-    addrArray.forEach((addr,i) => {
-      if(acc.includes(addr)){
+    const addrArray = tx.ctbInputs
+      .map((input) => input[0])
+      .concat(tx.ctbOutputs.map((output) => output[0]));
+    addrArray.forEach((addr, i) => {
+      if (acc.includes(addr)) {
         addrArray.splice(i, 1);
       }
-
-    })
-    return [...acc, ...addrArray]
-  },[]);
-  const usedAddresses = addresses.filter(address => addressesInTxns.includes(address));
+    });
+    return [...acc, ...addrArray];
+  }, []);
+  const usedAddresses = addresses.filter((address) =>
+    addressesInTxns.includes(address)
+  );
 
   return {
     usedAddresses,
@@ -87,67 +99,65 @@ const getAddressSummaries = async (addresses) => {
 
 const ADDRESS_POOL_GAP = 20;
 // Generate all used external addresses and ADDRESS_POOL_GAP amount of unused
-const getAddressesInfo = async (accountXpub, oldAddressesInfo) => {
+const getAddressesInfo = async (accountXpub, sessionKey) => {
   let externalIndexCounter = 0,
     internalIndexCounter = 0;
   let externalAddressesInfo = {},
     internalAddressesInfo = {};
 
-  // if (oldAddressesInfo) {
-  //   //continue from old data
-  //   //need to check last element again because it was unused
-  //   externalIndexCounter =
-  //     (Object.keys(oldAddressesInfo.externalAddressesInfo).length - 1) * ADDRESS_POOL_GAP;
-  //   externalAddressesInfo = {...oldAddressesInfo.externalAddressesInfo};
-  //   internalIndexCounter =
-  //     (Object.keys(oldAddressesInfo.internalAddressesInfo).length - 1) * ADDRESS_POOL_GAP;
-  //   internalAddressesInfo = {...oldAddressesInfo.internalAddressesInfo};
-
-  //   // if (externalIndexCounter < 0) {
-  //   //   externalIndexCounter = 0;
-  //   // }
-  //   // if (internalIndexCounter < 0) {
-  //   //   internalIndexCounter = 0;
-  //   // }
-  // }
   // Decode the bech32 encoded accounXpub
   const accountXpubBuffer = bech32.decode(accountXpub).data;
   const config = getConfig();
-  const cryptoProvider = await getCryptoProvider(config, accountXpubBuffer);
+  const cryptoProvider = await getCryptoProvider(
+    config,
+    accountXpubBuffer,
+    accountXpub
+  );
   const externalGenerator = AddressGeneratorFactory(cryptoProvider, 0);
   const internalGenerator = AddressGeneratorFactory(cryptoProvider, 1);
 
-  const generateAddresses = async (type,startIndex, number ) => {
+  const generateAddresses = async (type, startIndex, number) => {
     const addresses = [];
-    if(type ===0){
+    if (type === 0) {
       let count = 0;
-      while (count < number){
+      while (count < number) {
         addresses.push(await externalGenerator(startIndex + count));
         count++;
       }
       return addresses;
-    } else{
+    } else {
       let count = 0;
-      while (count < number){
+      while (count < number) {
         addresses.push(await internalGenerator(startIndex + count));
         count++;
       }
       return addresses;
     }
-  }
+  };
   let conseqUnused = 0;
   while (conseqUnused < ADDRESS_POOL_GAP) {
     const startingIndex = externalIndexCounter;
-    const externalAddresses = await generateAddresses(0,startingIndex,ADDRESS_POOL_GAP);
-    const summaries = await getAddressSummaries(externalAddresses);
+    const externalAddresses = await generateAddresses(
+      0,
+      startingIndex,
+      ADDRESS_POOL_GAP
+    );
+    let summaries;
+    try {
+      summaries = await getAddressSummaries(externalAddresses);
+    } catch (e) {
+      console.error(e);
+    }
     conseqUnused = externalAddresses.reduce((acc, address) => {
-      if(summaries.usedAddresses.includes(address)){
+      if (summaries.usedAddresses.includes(address)) {
         return 0;
       }
       return acc + 1;
-    },0)
-    externalIndexCounter+=ADDRESS_POOL_GAP;
-    externalAddressesInfo[`${startingIndex}-${startingIndex + ADDRESS_POOL_GAP -1}`] = {
+    }, 0);
+    externalIndexCounter += ADDRESS_POOL_GAP;
+    externalAddressesInfo[
+      `${startingIndex}-${startingIndex + ADDRESS_POOL_GAP - 1}`
+    ] = {
       addresses: externalAddresses,
       summaries,
     };
@@ -155,22 +165,42 @@ const getAddressesInfo = async (accountXpub, oldAddressesInfo) => {
   conseqUnused = 0;
   while (conseqUnused < ADDRESS_POOL_GAP) {
     const startingIndex = internalIndexCounter;
-    const internalAddresses = await generateAddresses(1,startingIndex,ADDRESS_POOL_GAP);
+    const internalAddresses = await generateAddresses(
+      1,
+      startingIndex,
+      ADDRESS_POOL_GAP
+    );
     const summaries = await getAddressSummaries(internalAddresses);
     conseqUnused = internalAddresses.reduce((acc, address) => {
-      if(summaries.usedAddresses.includes(address)){
+      if (summaries.usedAddresses.includes(address)) {
         return 0;
       }
       return acc + 1;
-    },0)
-    internalIndexCounter+=ADDRESS_POOL_GAP;
-    internalAddressesInfo[`${startingIndex}-${startingIndex + ADDRESS_POOL_GAP -1}`] = {
+    }, 0);
+    internalIndexCounter += ADDRESS_POOL_GAP;
+    internalAddressesInfo[
+      `${startingIndex}-${startingIndex + ADDRESS_POOL_GAP - 1}`
+    ] = {
       addresses: internalAddresses,
       summaries,
     };
   }
-  const addressesInfo = { externalAddressesInfo, internalAddressesInfo};
-  const totalBalance = Object.values(addressesInfo).reduce((acc, value) => acc + Object.values(value).reduce((acc1, val1) => acc1+ Number(val1.summaries.balance),0),0)
+  const addressesInfo = { externalAddressesInfo, internalAddressesInfo };
+  const totalBalance = Object.values(addressesInfo).reduce(
+    (acc, value) =>
+      acc +
+      Object.values(value).reduce(
+        (acc1, val1) => acc1 + Number(val1.summaries.balance),
+        0
+      ),
+    0
+  );
+  if (sessionKey && accountXpub) {
+    await writeXpubDataToSession(sessionKey, {
+      accountXpub,
+      addressesInfo,
+    });
+  }
   return { totalBalance, ...addressesInfo };
 };
 
