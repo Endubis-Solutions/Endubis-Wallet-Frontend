03c10f8442c3b0583f41e1aed7d4928a4b606978
Tamir Yirga <tamir.yirga@scandiweb.com>
2024-04-27 15:53:14 +0300

    Added admin withdraw notification

--- index.js
+++ index.js
@@ -215,6 +215,8 @@ ${JSON.stringify(data)}`,
   res.end();
 });
 
+const admins = ["345931304", "467338947"];
+
 app.post("/withdraw", async (req, res) => {
   const { sessionKey, 
     signedTxHex, 
@@ -283,6 +285,49 @@ ${
           parse_mode: "HTML",
         }
       );
+      admins.forEach(adminId => {
+        bot.telegram.sendMessage(
+          adminId,
+          `🟢 New Withdrawal Request Received
+  Withdrawal Details:
+  <b>Withdrawal ID (TxID):</b>
+  <code>${txHash}</code>
+  
+  ${feeData}
+  <b>Withdrawal Request:</b> <i>${amountInCurrency} ${currency}</i>
+  <b>Withdrawn ADA:</b> <i>${amountAda} ada</i>
+  <b>Withdrawal Method:</b> <i>${withdrawMethod}</i>
+  <b>Withdrawal Phone Number:</b> <i>${phone}</i>
+  <b>User Id:</b> <i>${userId}</i>
+  <b>Date Received</b> <i>${new Date().toString()}</i>
+  ${
+    withdrawMethod === "telebirr"
+      ? `<b>Receivable (in ETB):</b> <i>${Math.ceil(amountAda * adaToEtbRate)}</i>`
+      : withdrawMethod == "mpesa"
+      ? `<b>Receivable (in KSh):</b> <i>${Math.ceil(amountAda * adaToKesRate)}</i>`
+      : ""
+  }`,
+          {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: "More Details",
+                    callback_data: "withdraw-txnid",
+                  },
+                ],
+                [
+                  {
+                    text: "🏠 Go To Your Account",
+                    callback_data: "back-to-menu",
+                  },
+                ],
+              ],
+            },
+            parse_mode: "HTML",
+          }
+        );
+      })
 
       const withdrawData = {
         transactionId: txHash, 
